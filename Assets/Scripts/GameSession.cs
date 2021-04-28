4621d4dff3e9b0b7e57339e90695f8eb66ba6a73
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-04-28 16:01:46 +0300

    tried to build and failed cuz of gradle. Commented out CLIK Plugin lines

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using Tabtale.TTPlugins;
+//using Tabtale.TTPlugins;
 
 public class GameSession : MonoBehaviour
 {
